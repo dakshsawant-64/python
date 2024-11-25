b589e2d61d127d37a7158ff7f96331b42969dd1a
dakshsawant-64 <dakshssawant@gmail.com>
2024-11-25 18:10:44 +0530

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,21 +1,27 @@
-"1 or 2" = 1
-while 1 < 3
-    while "1 or 2 "== 2
-       "quit or not" = input("> ")
-        exit() 
-    "1 or 2" == 2
-    "no:1" = input("> ")
-    "no:2" = input("> ")
-    "symbol" = input("> ")
-    if symbol == '+'
-        add = no:1 + no:2
-        print(add)
-    if symbol == '/'
-        '+' = no:1 / no:2
-        print(/)
-    if symbol == '-'
-        '-' = no:1 - no:2
-        print(-)
-    if symbol == '*'
-        '*' = no:1 * no:2
-        print(*)
+e = 1
+while True:
+    quitornot = input("> Do you want to quit? (yes/no): ").strip().lower()
+    if quitornot == 'yes':
+        print("Exiting the calculator. Goodbye!")
+        break
+    noone = input("> Enter first number: ")
+    notow = input("> Enter second number: ")
+    symbol = input("> Enter operation (+, -, *, /): ")
+    try:
+        num1 = int(noone)
+        num2 = int(notow)
+        if symbol == '+':
+            print(f"Result: {num1 + num2}")
+        elif symbol == '-':
+            print(f"Result: {num1 - num2}")
+        elif symbol == '*':
+            print(f"Result: {num1 * num2}")
+        elif symbol == '/':
+            if num2 != 0:
+                print(f"Result: {num1 / num2}")
+            else:
+                print("Error: Cannot divide by zero.")
+        else:
+            print("Error: Invalid operation. Please use +, -, *, or /.")
+    except ValueError:
+        print("Error: Invalid input. Please enter valid numbers.")
